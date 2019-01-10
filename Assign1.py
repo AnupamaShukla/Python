37767e28f6e718ee4c3387a9f63874d49a1069c6
AnupamaShukla <46437052+AnupamaShukla@users.noreply.github.com>
2019-01-10 17:31:59 +0530

    Create Assign1.py

---
+++ Assign1.py
@@ -0,0 +1,11 @@
+print("Enter first number : ")
+Number1 = int (input())
+
+print("Enter second number : ")
+Number2 = int (input())
+
+print("Adition of numbers is : " + str(Number1 + Number2))
+print("Subtraction of numbers is : " + str(Number1 - Number2))
+print("Multiplication of numbers is : " + str(Number1 * Number2))
+print("Division of numbers is : " + str(Number1 / Number2))
+print("Exponential of numbers is : " + str(Number1 ** Number2))
