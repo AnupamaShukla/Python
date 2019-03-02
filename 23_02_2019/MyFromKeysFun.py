24137153ae59136ee80297d1e4255b578469b8ec
AnupamaShukla <46437052+AnupamaShukla@users.noreply.github.com>
2019-03-03 00:23:27 +0530

    Update MyFromKeysFun.py

--- 23_02_2019/MyFromKeysFun.py
+++ 23_02_2019/MyFromKeysFun.py
@@ -2,10 +2,15 @@ def FromKeys(Input, value):
     if (type(value)) == list:
         y = 0
         for key in Input:
+            if(y >= len(value)):
+                Input[key] = None
+                continue
             Input[key] = value[y]
-            if len(value) > 1:
-                y += 1
+            y += 1
         return Input
+    else:
+        res = dict.fromkeys(Input,value)
+        return res
     
 def main():
     inputDict = eval (input("Enter Key value in dict:"))
